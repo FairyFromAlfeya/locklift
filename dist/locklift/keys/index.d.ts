77d8ed899c21f48c34f7fb6d1b58f86bc466b1b4
Александр Кунеков <fairyfromalfeya@gmail.com>
2022-05-24 23:13:16 +0200

    v1.0.5

--- dist/locklift/keys/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Locklift from '../index';
-export declare class Keys {
-    locklift: Locklift;
-    keyPairs: any[];
-    constructor(locklift: any);
-    getKeyPairs(): Promise<any[]>;
-    setup(): Promise<void>;
-}
